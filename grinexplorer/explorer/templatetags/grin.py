331d1128eb3f996e60f15496c1a318f83f88b182
Hendrik Richter <hendi@gasket.io>
2018-03-27 10:26:30 +0200

    make units consistent, fix #10

---
+++ grinexplorer/explorer/templatetags/grin.py
@@ -0,0 +1,50 @@
+from decimal import Decimal
+from django import template
+
+register = template.Library()
+
+
+def format_float(f):
+    s = "{:,f}".format(f)
+    if "." not in s:
+        return s
+
+    return s.rstrip("0").rstrip(".")
+
+
+@register.filter
+def nanogrin(nanogrin):
+    if nanogrin == 0:
+        return grin(0)
+
+    if nanogrin < 1000:
+        return "%d ng" % nanogrin
+
+    return microgrin(Decimal(nanogrin) / Decimal(1000))
+
+
+@register.filter
+def microgrin(microgrin):
+    if microgrin == 0:
+        return grin(0)
+
+    if microgrin < 1000:
+        return "%s µg" % format_float(microgrin)
+
+    return milligrin(Decimal(microgrin) / Decimal(1000))
+
+
+@register.filter
+def milligrin(milligrin):
+    if milligrin == 0:
+        return grin(0)
+
+    if milligrin < 1000:
+        return "%s mg" % format_float(milligrin)
+
+    return grin(Decimal(milligrin) / Decimal(1000))
+
+
+@register.filter
+def grin(grin):
+    return "%s grin" % format_float(grin)
