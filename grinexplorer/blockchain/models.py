af9de51617cea204f805a46f769dd156e871575f
Quentin Le Sceller <q.lesceller@gmail.com>
2018-07-10 09:58:49 -0700

    Update difficulty calculation

--- grinexplorer/blockchain/models.py
+++ grinexplorer/blockchain/models.py
@@ -43,7 +43,7 @@ class Block(models.Model):
 
     @property
     def difficulty(self):
-        return 0xffffffffffffffff // int(self.hash[:16], 16)
+        return 0xffffffffffffffff // int(self.hash[:16], 16) * (1<<(self.cuckoo_size-30))*(self.cuckoo_size-1)
 
     @property
     def target_difficulty(self):
