63658013bafde3675e85411897c1abb4ce0d1929
Hendrik Richter <info@prokapi.com>
2019-01-04 22:38:58 +0100

    better error when json deserialization fails

--- grinexplorer/blockchain/management/commands/import_from_tip.py
+++ grinexplorer/blockchain/management/commands/import_from_tip.py
@@ -1,3 +1,4 @@
+import json
 from enum import Enum
 
 import requests
@@ -27,7 +28,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.API_BASE = "%s/v1/" % options["url"]
 
-        data = requests.get(self.API_BASE + "chain").json()
+        resp = requests.get(self.API_BASE + "chain")
+
+        try:
+            data = resp.json()
+        except json.decoder.JSONDecodeError:
+            print("Decoding JSON failed (did you set `archive_mode=true` in grin-server.toml?")
+            print("resp=%r" % resp.text)
+            exit()
+
         height = data["height"]
         tip = data["last_block_pushed"]
         self.stdout.write("height={}, tip={}\n\n".format(height, tip))
