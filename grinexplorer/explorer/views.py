483937078d5ab974592e44f3b8d9e297503a052a
Hendrik Richter <info@prokapi.com>
2019-01-07 14:16:39 +0100

    don't crash on empty Block database

--- grinexplorer/explorer/views.py
+++ grinexplorer/explorer/views.py
@@ -162,23 +162,24 @@ class BlockList(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["highest_block"] = Block.objects.order_by("height").last()
-        context["latest_block"] = Block.objects.order_by("timestamp").last()
-        context["total_emission"] = Block.objects.order_by("total_difficulty").last().height * 60
+        if Block.objects.exists():
+            context["highest_block"] = Block.objects.order_by("height").last()
+            context["latest_block"] = Block.objects.order_by("timestamp").last()
+            context["total_emission"] = Block.objects.order_by("total_difficulty").last().height * 60
 
-        context["competing_chains"] = Block.objects \
-                                           .filter(height__gte=context["highest_block"].height - 100) \
-                                           .values("height") \
-                                           .annotate(cnt=Count("height")) \
-                                           .aggregate(Max("cnt"))["cnt__max"]
-        context["forked_at"] = Block.objects \
-                                    .filter(height__gte=context["highest_block"].height - 60) \
-                                    .values("height") \
-                                    .annotate(cnt=Count("height")) \
-                                    .filter(cnt__gt=1) \
-                                    .aggregate(Min("height"))["height__min"]
+            context["competing_chains"] = Block.objects \
+                                               .filter(height__gte=context["highest_block"].height - 100) \
+                                               .values("height") \
+                                               .annotate(cnt=Count("height")) \
+                                               .aggregate(Max("cnt"))["cnt__max"]
+            context["forked_at"] = Block.objects \
+                                        .filter(height__gte=context["highest_block"].height - 60) \
+                                        .values("height") \
+                                        .annotate(cnt=Count("height")) \
+                                        .filter(cnt__gt=1) \
+                                        .aggregate(Min("height"))["height__min"]
 
-        context['thumb_chart_list'] = [self.get_block_chart(), self.get_fee_chart()]
+            context['thumb_chart_list'] = [self.get_block_chart(), self.get_fee_chart()]
 
         return context
 
